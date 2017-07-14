871d36a67a6d0d9c95bfb2cf13713b9e79d77f0c
liznomura <lizn@hawaii.edu>
2017-07-13 15:41:13 -1000

    deleted some commented out lines

--- js/client.js
+++ js/client.js
@@ -15,12 +15,4 @@ const client = net.createConnection(options, () => {
 client.on('data', data => {
   console.log(data.toString());
 });
-
-  //Whenever the connected socket (client) emits a 'data' event, then data is being broadcasted from the server, pipe that data out to your terminal's standard output stream.
-
-  //   process.stdin.pipe(process.stdout);
-
-  // client.on('end', () => {
-  //   console.log('client disconnected');
-  // });
 });
